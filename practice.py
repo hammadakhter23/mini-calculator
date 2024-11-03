31ff082bce2a8bbc60fba0c84f121547a18f92e6
Hammad Akhtar <hammadakhter924@gmail.com>
2024-11-03 12:13:22 -0800

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,24 @@
+# Simple Calculator in Python
+
+# Take user inputs
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Enter operation (+, -, *, /): ")
+
+# Perform calculation based on operation
+if operation == '+':
+    result = num1 + num2
+elif operation == '-':
+    result = num1 - num2
+elif operation == '*':
+    result = num1 * num2
+elif operation == '/':
+    if num2 != 0:
+        result = num1 / num2
+    else:
+        result = "Error! Division by zero."
+else:
+    result = "Invalid operation."
+
+# Print result
+print("Result:", result)
